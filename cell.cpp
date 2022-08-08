f7529cf55ba62c505b5233a78e738d3f4225f0e7
YaImedgar <ya@imedgar.ru>
2022-08-09 00:16:55 +0300

    add cell class with Set\Clear\GetValue\GetText methods

---
+++ cell.cpp
@@ -0,0 +1,87 @@
+#include "cell.h"
+
+#include <cassert>
+#include <iostream>
+#include <string>
+#include <optional>
+
+void Cell::Set(std::string text)
+{
+    if (text.empty()) // empty
+    {
+        impl_ = std::make_unique<EmptyImpl>();
+        return;
+    }
+
+    if (text.size() > 1 && text[0] == FORMULA_SIGN)
+    {
+        impl_ = std::make_unique<FormulaImpl>(text);
+        return ;
+    }
+
+    impl_ = std::make_unique<TextImpl>(text);
+}
+
+void Cell::Clear()
+{
+    impl_.release();
+}
+
+Cell::Value Cell::GetValue() const
+{
+    return impl_->GetValue();
+}
+
+std::string Cell::GetText() const
+{
+    return impl_->GetText();
+}
+
+Impl::Value EmptyImpl::GetValue() const
+{
+    return std::string{};
+}
+
+std::string EmptyImpl::GetText() const
+{
+    return std::string();
+}
+
+FormulaImpl::FormulaImpl(std::string expression)
+{
+    formula_ = ParseFormula(expression.substr(1));
+}
+
+Impl::Value FormulaImpl::GetValue() const
+{
+    auto raw_value = formula_->Evaluate();
+    if (std::holds_alternative<double>(raw_value))
+    {
+        return std::get<double>(raw_value);
+    }
+    return std::get<FormulaError>(raw_value);
+}
+
+std::string FormulaImpl::GetText() const
+{
+    return FORMULA_SIGN + formula_->GetExpression();
+}
+
+TextImpl::TextImpl(std::string expression)
+    : expression_(std::move(expression))
+{}
+
+Impl::Value TextImpl::GetValue() const
+{
+    if (expression_[0] == ESCAPE_SIGN)
+    {
+        return expression_.substr(1);
+    }
+
+    return expression_;
+}
+
+std::string TextImpl::GetText() const
+{
+    return expression_;
+}
---
+++ cell.h
@@ -0,0 +1,53 @@
+#pragma once
+
+#include "common.h"
+#include "formula.h"
+
+class Impl
+{
+public:
+    using Value = CellInterface::Value;
+    virtual ~Impl() = default;
+    virtual Value GetValue() const = 0;
+    virtual std::string GetText() const = 0;
+};
+
+class EmptyImpl final : public Impl
+{
+public:
+    Value GetValue() const override;
+    std::string GetText() const override;
+};
+
+class TextImpl final : public Impl
+{
+public:
+    explicit TextImpl(std::string expression);
+    Value GetValue() const override;
+    std::string GetText() const override;
+private:
+    std::string expression_;
+};
+
+class FormulaImpl final : public Impl
+{
+public:
+    explicit FormulaImpl(std::string expression);
+    Value GetValue() const override;
+    std::string GetText() const override;
+private:
+    std::unique_ptr<FormulaInterface> formula_;
+};
+
+class Cell final : public CellInterface {
+public:
+    Cell() = default;
+    ~Cell() = default;
+    void Set(std::string text) override;
+    void Clear();
+
+    Value GetValue() const override;
+    std::string GetText() const override;
+private:
+    std::unique_ptr<Impl> impl_;
+};
